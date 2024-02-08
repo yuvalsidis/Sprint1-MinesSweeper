4a0eb39b710cdc7c0db825f3af2591ef29eb41cd
yuvalsidis <75839453+yuvalsidis@users.noreply.github.com>
2024-02-08 15:07:22 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -9,14 +9,18 @@ const WIN = '😎'
 const MARK = '🧨'
 const LEFTCLICK = 1
 const RIGHTCLICK = 2
+const negsAround = ['1️⃣', '2️⃣', '3️⃣']
+const MINEBOMBED = '☠️'
+
 
 var gCurrectLevel = null
-var gIsGameOn = false
+var isOn = false
 var gLose = false
 var gWin = false
 var gNumbersOfMines = null
 var gNumbersOfEmptyCell = null
 var gBoard = null
+var gLives = 0
 
 var gGame = {
     isOn: false,
@@ -35,14 +39,26 @@ function onInit() {
     console.log(gBoard)
     renderBoard()
     enableRightClickOnContainer()
+    renderMarkCount(gGame.markedCount)
+    renderLivesCount(gLives)
+    initVariables()
 }
 
+function initVariables() {
+    gGame.shownCount = 0
+    gGame.secPassed = 0
+    isOn = true
+    gLives = 2 // will change to 3 when have more mines
+    gWin = false
+    gLose = false
+}
 
 //build a board
 function buildBoard() {
     const cols = gCurrectLevel.cols
     const rows = gCurrectLevel.rows
     const mines = gCurrectLevel.mines
+    gGame.markedCount = gCurrectLevel.mines
     const board = []
     for (var i = 0; i < rows; i++) {
         board.push([])
@@ -59,6 +75,8 @@ function buildBoard() {
     // Will Change to be Randomaly and not manually
     board[0][2].isMine = true
     board[1][3].isMine = true
+    board[0][2].isShown = false
+    board[1][3].isShown = false
     return board
 
 }
@@ -97,10 +115,10 @@ function renderBoard() {
             const className = `cell cell-${i}-${j}`
             const cellId = makeId(length = 6)
             if (cell.isMine) {
-                strHTML += `<td class="${className}">  <button id="${cellId}"   
-                onclick="onLeftClicked(event, ${i}, ${j})"
-                oncontextmenu="onCellRightClicked(event, ${i}, ${j})" 
-                class="cellButton">${MINE}</button> 
+                strHTML += `<td class="${className}"><button id="${cellId}"   
+                onclick="onCellLeftClicked(this, ${i}, ${j})"
+                oncontextmenu="onCellRightClicked(this, ${i}, ${j})" 
+                class="cellButton"></button> 
                 </td>`
                 gNumbersOfMines++
             }
@@ -125,69 +143,168 @@ function renderBoard() {
 
 function onCellLeftClicked(elCell, i, j) {
     console.log(`left Click on cell ${i}, ${j}`)
-    handleRightClick(elCell, i, j)
+    handleLeftClick(elCell, i, j)
+}
 
+function handleLeftClick(elCell, i, j) {
+    const cell = gBoard[i][j]
+    if(gLives === 0) return
+
+    // need to add bombedMine in the same place
+    if (cell.isMine) {
+        //MODEL
+        gLives--
+        //DOM
+        renderLivesCount(gLives)
+        renderButton({ i: i, j: j }, MINEBOMBED)
+    } else {
+        if (cell.minesAroundCount > 0) {
+            onCallShown(i, j)
+        }
+        else if (cell.minesAroundCount === 0) {
+            expandShown(i, j)
+        }
+    }
+    gLives === 0 ? handleLose(i,j) : null
+}
+
+function handleLose(i,j) {
+    reavelAllMines(i,j)
+    isOn = false
+
+
+    //Restart button appear
+    // pop up of loser for few seconds interval then hide it
+
+
+}
+
+function reavelAllMines(i,j) {
+    for (var rows = 0; rows< gBoard.length; rows++) {
+        for (var cols = 0; cols < gBoard[0].length; cols++) {
+            const currCell = gBoard[rows][cols]
+            if(currCell.isMine && !currCell.isShow){
+                //MODEL
+                currCell.isShow = true
+                //DOM
+                renderButton({ i: i, j: j }, MINE)
+            }
+        }
+    }
+}
+
+
+function onCallShown(i, j) {
+    const cell = gBoard[i][j]
+    if (!cell.isShown && !cell.isMine && !cell.isMarked) {
+        var value
+        //model
+        cell.isShown = true
+        //dom
+        switch (cell.minesAroundCount) {
+            case 1:
+                value = negsAround[0]
+                break;
+            case 2:
+                value = negsAround[1]
+                break;
+            case 3:
+                value = negsAround[2]
+                break;
+        }
+        renderButton({ i: i, j: j }, value)
+    }
+}
+
+function expandShown(rows, cols) {
+    for (var i = rows - 1; i <= rows + 1; i++) {
+        if (i < 0 || i >= gBoard.length) continue
+        for (var j = cols - 1; j <= cols + 1; j++) {
+            if (i === rows && j === cols) continue
+            if (j < 0 || j >= gBoard[0].length) continue
+            const currCell = gBoard[i][j]
+            if (currCell.minesAroundCount === 0) continue
+            onCallShown(i, j)
+        }
+    }
 }
 
 function onCellRightClicked(elCell, i, j) {
     console.log(`right Click on cell ${i}, ${j}`)
+    handleRightClick(elCell, i, j)
 }
 
 function handleRightClick(elCell, i, j) {
     onCelMarked(elCell, i, j)
 }
 
-function handleLeftClick() {
-}
 
 function onCelMarked(elCell, i, j) {
     const cell = gBoard[i][j]
-    const objectCell = {i : i, j: j}
-    if (cell.isMarked === false && cell.isShown === false) {
-        renderCell(objectCell, MARK)
+    // if (cell.isMine === true && cell.isShown === true) return
+    if (cell.isShown === true) return
+    if (!cell.isMarked && !cell.isShow) {
+        if (gGame.markedCount === 0) return
+        //MODEL
+        cell.isMarked = true
+        //DOM
+        renderButton({ i: i, j: j }, MARK)
+        updateMarkCount(true)
+        console.log(cell)
+        console.log(gGame.markedCount)
+    }
+    else if (cell.isMarked && !cell.isShow) {
+        //MODEL
+        cell.isMarked = false
+        //DOM
+        renderButton({ i: i, j: j }, '')
+        console.log(cell)
+        console.log(gGame.markedCount)
+        updateMarkCount(false)
+    }
+
+
+}
+
+
+function updateMarkCount(condition) {
+    if (condition) {
+        //MODEL
+        gGame.markedCount--
+        //DOOM
+        renderMarkCount(gGame.markedCount)
+    }
+    else {
+        //MODEL
+        gGame.markedCount++
+        //GAME
+        renderMarkCount(gGame.markedCount)
     }
 
 }
 
 
+function renderTime(value) {
+    const elm = document.querySelector('.timeDisplay').querySelector('span')
+    elm.innerHTML = value
+}
 
+function renderLivesCount(value) {
+    const elm = document.querySelector('.livesCountDisplay').querySelector('span')
+    elm.innerHTML = value
+}
 
-
-/*function onCellClicked(elCell, i, j) {
-
-}*/
-
-
-//Enable right click on the container
-function enableRightClickOnContainer() {
-    document.getElementById("container").addEventListener("contextmenu", function (event) {
-        event.preventDefault();
-
-    })
-
+function renderMarkCount(value) {
+    const elm = document.querySelector('.marksCountDisplay').querySelector('span')
+    elm.innerHTML = value
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function renderButton(location, value) {
+    const tdElm = document.querySelector(`.cell-${location.i}-${location.j}`)
+    const btn = tdElm.querySelector('button')
+    btn.innerHTML = value
+}
 
 
 function renderCell(location, value) {
@@ -197,6 +314,14 @@ function renderCell(location, value) {
 }
 
 
+
+//Enable right click on the container
+function enableRightClickOnContainer() {
+    document.getElementById("container").addEventListener("contextmenu", function (event) {
+        event.preventDefault();
+    })
+
+}
 
 function makeId(length = 6) {
     var txt = ''
